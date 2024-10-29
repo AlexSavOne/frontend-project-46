72846d7efaf0a0cfcf2a64580c958d84839f2b66
AlexS <OstinKarlail@yandex.ru>
2024-10-29 13:01:42 +0500

    feat: добавил поддержку аргументов и опции формата

--- gendiff.js
+++ gendiff.js
@@ -5,6 +5,13 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(`Comparing ${filepath1} and ${filepath2}`);
+  });
+
+
 
 program.parse(process.argv);
